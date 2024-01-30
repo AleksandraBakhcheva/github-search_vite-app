7e55d9e363a82d99ccd100cdcf98f1eb59bc3779
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-01-31 00:31:11 +0300

    add store

---
+++ src/assets/stores/repos-store.js
@@ -0,0 +1,40 @@
+import { makeAutoObservable } from "mobx";
+import axios from "axios";
+
+class ReposStore {
+  repos = [];
+  favourites = [];
+  isLoading = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  loadDataAction = async (value) => {
+    if (value) {
+      this.isLoading = true;
+      const res = await axios.get(
+        `https://api.github.com/search/repositories?q=${value}`,
+        {
+          params: {
+            _limit: 20,
+          },
+        }
+      );
+      this.repos = res.data;
+      this.isLoading = false;
+    }
+  };
+
+  addToFavourites = (id) => {
+    console.log(id);
+    this.favourites.push(id);
+  };
+
+  deleteFromFavourites = (id) => {
+    this.favourites.pop(id);
+  };
+}
+
+const store = new ReposStore();
+export default store;
