83b53126d83f7cc92a895fd976bffed5c49e16ad
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-01-31 16:56:26 +0300

    add Layout and App components

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Provider } from "mobx-react";
+import store from "./assets/stores/repos-store";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Layout } from "./assets/components/Layout";
+import { MainPage } from "./assets/pages/MainPage";
+import { RepoPage } from "./assets/pages/RepoPage";
+import { NotFoundPage } from "./assets/components/NotFoundPage";
+
+function App() {
+  return (
+    <Provider {...store}>
+      <>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="details/:id" element={<RepoPage />} />
+            <Route path="404" element={<NotFoundPage />} />
+            <Route path="*" element={<Navigate replace to="404" />} />
+          </Route>
+        </Routes>
+      </>
+    </Provider>
+  );
+}
+
+export default App;
